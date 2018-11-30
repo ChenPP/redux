0f51bac57aaccf3a7e39be10605dbf859d1e3d93
redCPP <botwin717@gmail.com>
2018-11-30 19:02:19 +0800

    redux
    redux基本

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { creatStore } from 'redux';
+import { createStore } from 'redux';
 
 import './styles.css';
 
-function App() {
+const actionNewArray = {
+  type: 'new_a_array',
+  data: [1, 2, 3, 4]
+};
+
+const reducer = (state = [], action) => {
+  if (action.type === 'new_a_array') {
+    state = [...state, action.data];
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+
+store.dispatch(actionNewArray);
+console.log(store.getState());
+
+const App = () => {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
